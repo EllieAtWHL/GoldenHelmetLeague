53fd39e6374f8c3f38c5f0d44a858d89aaf63578
EllieAtWHL <eleanormatthewman@gmail.com>
2023-08-05 14:48:18 +0100

    ghl-3 moving the draft board to it's own component

---
+++ force-app/main/default/lwc/draftBoard/draftBoard.js
@@ -0,0 +1,30 @@
+import { LightningElement, api } from 'lwc';
+
+const SNAKE = 'snake';
+const AUCTION = 'auction';
+
+export default class DraftBoard extends LightningElement {
+
+    @api teams;
+    @api draft;
+
+    get isSnake(){
+        return this.draft.draftType === SNAKE;
+    }
+
+    get isAuction(){
+        return this.draft.draftType === AUCTION;
+    }
+
+    get auctionDraft(){this
+        let tempAuction = JSON.parse(JSON.stringify(this.draft));
+        tempAuction.teams = this.teams;
+        console.log(JSON.stringify(tempAuction));
+        return tempAuction;
+    }
+
+    get draftInfo(){
+        return isSnake ? this.draft : this.auctionDraft();
+    }
+
+}
