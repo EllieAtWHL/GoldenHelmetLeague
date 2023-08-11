f969ef16774865524c80853bbb8ee01d32a1f4c7
EllieAtWHL <eleanormatthewman@gmail.com>
2023-08-11 23:21:04 +0100

    GHL-7: managing session id for guest user

--- force-app/main/default/lwc/cometD/cometD.js
+++ force-app/main/default/lwc/cometD/cometD.js
@@ -2,6 +2,7 @@ import { LightningElement, track, wire, api } from 'lwc';
 import { loadScript } from "lightning/platformResourceLoader";
 import cometd from "@salesforce/resourceUrl/cometd";
 import getSessionId from '@salesforce/apex/CometdController.getSessionId';
+import getStoredSessionId from '@salesforce/apex/CometdController.getStoredSessionId';
 
 export default class CometD extends LightningElement {
     @api channel;
@@ -24,9 +25,22 @@ export default class CometD extends LightningElement {
                 })
 
         } else if (result.error) {
-            console.log(result.error);
             this.error = result.error;
             this.sessionId = undefined;
+            console.log(result.error);
+        } else {
+            getStoredSessionId()
+                .then( result => {
+                    this.sessionId = result;
+                    loadScript(this, cometd)
+                        .then(() => {
+                            this.initCometd()
+                        })
+                        .catch(error => {
+                            console.log(error);
+                        })
+                })
+                
         }
     }
 
@@ -53,8 +67,7 @@ export default class CometD extends LightningElement {
                     lwcThisContext.dispatchEvent(selectedEvent);
                 });
             } else {
-                // console.error('Error in handshaking: ' + JSON.stringify(status));
-                
+                console.error('Error in handshaking: ' + JSON.stringify(status));
             }
         });
     }
