65aa4d357636d5bb279bb492d2c3e19b753cdca9
EllieAtWHL <eleanormatthewman@gmail.com>
2023-08-24 16:10:39 +0100

    managing display screen for unused auction picks

--- force-app/main/default/lwc/auctionTeamView/auctionTeamView.js
+++ force-app/main/default/lwc/auctionTeamView/auctionTeamView.js
@@ -2,4 +2,19 @@ import { LightningElement, api } from 'lwc';
 
 export default class AuctionTeamView extends LightningElement {
     @api team;
+
+    get teamPicks(){
+        let tempPicks = JSON.parse(JSON.stringify(this.team.picks));
+        tempPicks.forEach( pick => {
+            if(pick.playerPickedName.length > 15) {
+                let tempName = pick.playerPickedName.split(',',1)[0];
+                pick.playerPickedName = tempName;
+            }
+        })
+        while(tempPicks.length < 17){
+            tempPicks.push({cssClass: 'pick-made'});
+        }
+        
+        return tempPicks;
+    }
 }
